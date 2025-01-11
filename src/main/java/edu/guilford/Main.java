35f3069c00f38bf73b9be55ee61ea5f62b4c4bad
ayaamghi <ayaamghi@gmail.com>
2025-01-10 22:44:53 -0500

    done

--- src/main/java/edu/guilford/Main.java
+++ src/main/java/edu/guilford/Main.java
@@ -1,7 +1,17 @@
 package edu.guilford;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Password password1 = new Password(5, true, true, true, true);
+        ArrayList<Password> passwords = new ArrayList<Password>();
+        Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            passwords.add(new Password(random.nextInt(1, 11), random.nextBoolean(), random.nextBoolean(), random.nextBoolean(), random.nextBoolean()));
+        }
+        System.out.println(passwords); 
     }
 }
